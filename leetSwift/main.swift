84c295e9dcce1e7a120782714372dfc13e840e38
r0am3r <r0am3r@protonmail.com>
2020-03-22 09:09:57 +0800

    Initial Commit

---
+++ leetSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetSwift
+//
+//  Created by Wong Feng on 22/3/2020.
+//  Copyright © 2020 Wong Feng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
